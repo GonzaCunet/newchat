f877e840e0b877b7871d6e4bf1465a2ba0be36cd
Gonza <gonza.cunet@outlook.com>
2024-05-20 12:37:18 -0300

    prueba sin env

--- front/rtdb.js
+++ front/rtdb.js
@@ -5,7 +5,7 @@ const firebase_1 = require("firebase");
 exports.API_BASE_URL = process.env.ENVIRONMENT == "DEV"
     ? "http://localhost:3000"
     : "https://newchat-za33.onrender.com";
-const app = firebase_1.default.initializeApp({
+firebase_1.default.initializeApp({
     apiKey: "AIzaSyACD-PFhB7oZMNpkDK2JOxc0Rsqwdk_Pc8",
     authDomain: "el-fairbase.firebaseapp.com",
     databaseURL: "https://el-fairbase-default-rtdb.firebaseio.com",
