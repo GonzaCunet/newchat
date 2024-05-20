dc95a889fd193194e6c400838b010327b333dfd2
Gonza <gonza.cunet@outlook.com>
2024-05-20 12:17:41 -0300

    API BASE URL

--- front/rtdb.ts
+++ front/rtdb.ts
@@ -1,10 +1,8 @@
 import firebase from "firebase";
 
-export const API_BASE_URL = "http://localhost:3000";
-const app = firebase.initializeApp({
-  apiKey: "AIzaSyACD-PFhB7oZMNpkDK2JOxc0Rsqwdk_Pc8",
-  authDomain: "el-fairbase.firebaseapp.com",
-  databaseURL: "https://el-fairbase-default-rtdb.firebaseio.com",
-});
+export const API_BASE_URL =
+  process.env.ENVIRONMENT == "DEV"
+    ? "https://localhost:3000"
+    : "https://newchat-za33.onrender.com";
 
 export const dataBase = firebase.database();
